7a1a09a7f55f3805168ad1961fedfbb06c35ea76
ali-mujtaba <am.alimujtaba@gmail.com>
2019-12-15 09:58:00 +0530

    Add support environment configurable server listening port

--- index.js
+++ index.js
@@ -573,7 +573,8 @@ app.get("*",function(req,res)
     res.redirect("/");
 });
 
-app.listen(3000,function()
+var port = process.env.PORT || 3000;
+app.listen(port,function()
 {
     console.log("Server is listening!");
 });
